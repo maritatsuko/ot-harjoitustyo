7c78a010d1d4ba97225154616376a38b4ed06d6b
Mari Lehtikari <mari.lehtikari@helsinki.fi>
2022-04-09 23:21:06 +0300

    loginscreen ui

--- src/ui/loginscreen.py
+++ src/ui/loginscreen.py
@@ -3,26 +3,24 @@ from tkinter import Tk, ttk, constants
 class LoginUI:
     def __init__(self, root):
         self._root = root
+        self._un_entry = None
     
     def start(self):
         heading_label = ttk.Label(master=self._root, text="Hi! Welcome to Mochi, please log in:")
         username_label = ttk.Label(master=self._root, text="Username: ")
-        un_entry = ttk.Entry(master=self._root)
-        button = ttk.Button(master=self._root, text="Log in")
-
-        #checkbutton = ttk.Checkbutton(master=self._root, text="Checkbutton")
-        #radiobutton = ttk.Radiobutton(master=self._root, text="Radiobutton")
+        self._un_entry = ttk.Entry(master=self._root)
+        button = ttk.Button(master=self._root, text="Log in", command=self._button_click)
         
         heading_label.grid(row=0, column=0, columnspan=2, sticky=constants.W, padx=5, pady=5)
         username_label.grid(padx=5, pady=5)
-        un_entry.grid(row=1, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
+        self._un_entry.grid(row=1, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
         button.grid(row=3, column=0, columnspan=2, sticky=constants.W, padx=5, pady=5)
-       
-        #checkbutton.pack(side=constants.LEFT)
-        #radiobutton.pack(side=constants.RIGHT)
 
         self._root.grid_columnconfigure(1, weight=1, minsize=300)
 
+    def _button_click(self):
+       username = self._un_entry.get()
+       print(f"Welcome {username}")
 
 window = Tk()
 window.title("Mochi - Log in")
