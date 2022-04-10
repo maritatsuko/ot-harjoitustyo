56db97daf42bafb2370f4d2953f990bbfd82b4d9
Mari Lehtikari <mari.lehtikari@helsinki.fi>
2022-04-10 17:36:38 +0300

    loginscreen ui colors

--- src/ui/loginscreen.py
+++ src/ui/loginscreen.py
@@ -6,8 +6,8 @@ class LoginUI:
         self._un_entry = None
     
     def start(self):
-        heading_label = ttk.Label(master=self._root, text="Hi! Welcome to Mochi, please log in:")
-        username_label = ttk.Label(master=self._root, text="Username: ")
+        heading_label = ttk.Label(master=self._root, background="white", text="Hi! Welcome to Mochi, please log in:")
+        username_label = ttk.Label(master=self._root, background="white", text="Username: ")
         self._un_entry = ttk.Entry(master=self._root)
         button = ttk.Button(master=self._root, text="Log in", command=self._button_click)
         
@@ -16,7 +16,7 @@ class LoginUI:
         self._un_entry.grid(row=1, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
         button.grid(row=3, column=0, columnspan=2, sticky=constants.W, padx=5, pady=5)
 
-        self._root.grid_columnconfigure(1, weight=1, minsize=300)
+        self._root.grid_columnconfigure(1, weight=1, minsize=300)     
 
     def _button_click(self):
        username = self._un_entry.get()
@@ -24,6 +24,7 @@ class LoginUI:
 
 window = Tk()
 window.title("Mochi - Log in")
+window.configure(bg="pink")
 
 ui = LoginUI(window)
 ui.start()
