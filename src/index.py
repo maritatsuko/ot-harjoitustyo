1fff1fd08a77ea3b6f21fe1e8cb42aa673804147
Mari Lehtikari <mari.lehtikari@helsinki.fi>
2022-04-12 13:11:46 +0300

    trying to use imports, not working :(

---
+++ src/index.py
@@ -0,0 +1,15 @@
+from tkinter import Tk
+from ui.the_ui import UserInterface
+
+def main():
+    window = Tk()
+    window.title("Mochi")
+    window.configure(bg="pink")
+
+    ui = UserInterface(window)
+    ui.start()
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
