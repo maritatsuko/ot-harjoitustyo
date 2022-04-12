4fbd6ad1d332953248c439d1a6cb81b29925d5ff
Mari Lehtikari <mari.lehtikari@helsinki.fi>
2022-04-12 22:00:55 +0300

    invoke test

--- tasks.py
+++ tasks.py
@@ -5,6 +5,10 @@ def start(ctx):
     ctx.run("python3 src/ui/the_ui.py", pty=True)
 
 @task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+@task
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest", pty=True)
 
