ff50bddf970562fb6aed63e1653b3c5a8c046f06
Mari Lehtikari <mari.lehtikari@helsinki.fi>
2025-04-26 15:26:21 +0300

    add docstring

--- src/entities/piece.py
+++ src/entities/piece.py
@@ -1,6 +1,16 @@
 class Piece:
+    """Represents a piece of clothing with its attributes.
+    """
 
     def __init__(self, title: str, image_path: str, color: str, category: str):
+        """Class constructor for a new piece of clothing.
+
+        Args:
+            title (str): Name of the piece of clothing.
+            image_path (str): Path to the image of the piece.
+            color (str): Color of the piece.
+            category (str): Category of the piece (e.g., top, skirt, etc.).
+        """
         self.title = title
         self.image_path = image_path
         self.color = color
--- src/entities/user.py
+++ src/entities/user.py
@@ -1,5 +1,13 @@
 class User:
+    """Represents a user in the system.
+    """
 
     def __init__(self, username: str, password: str):
+        """Class constructor for a new user.
+
+        Args:
+            username (str): Username of the user.
+            password (str): Password of the user.
+        """
         self.username = username
         self.password = password
